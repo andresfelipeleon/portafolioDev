172423813713d65a2c70c39a4718034859d417fc
Andres León <aleonzuleta@gmail.com>
2025-05-27 16:52:03 -0500

    Ajuste de estilos, revisando la implementación de un navbar

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,11 +1,25 @@
 <script>
+import navbar from '../components/navbar.vue';
 
-
+export default {
+  name: 'Inicio',
+  components: {
+    navbar
+  },
+}
 </script>
 
 
 <template>
-<h1>Inicio</h1>
+  <navbar></navbar>
+
+<div class="container" id="inicio">
+    <h1>Bienvenido a la Aplicación</h1>
+    <p>Esta es una aplicación de ejemplo para demostrar el uso de Vue.js.</p>
+    <p>Utiliza el menú de navegación para explorar las diferentes secciones.</p>
+    <p>¡Disfruta navegando!</p>
+
+</div>
   </template>
 
 
